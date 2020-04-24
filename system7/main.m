cc427267d48f1e4b5cb55e333aeb6450ee40663c
Pavlo Shkrabliuk <pastey@readdle.com>
2020-04-24 09:11:01 +0300

    Initial Commit

---
+++ system7/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  system7
+//
+//  Created by Pavlo Shkrabliuk on 24.04.2020.
+//  Copyright © 2020 Readdle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
