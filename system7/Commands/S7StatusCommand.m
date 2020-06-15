e1c8aab2322f1c4376443f319ec361ff75140794
Pavlo Shkrabliuk <pastey@readdle.com>
2020-06-15 10:03:26 +0300

    refactor status command

--- system7/Commands/S7StatusCommand.m
+++ system7/Commands/S7StatusCommand.m
@@ -61,8 +61,22 @@
         puts("Subrepos status:");
     }
 
-    NSDictionary<NSNumber * /* S7Status */, NSSet<NSString *> *> *status = nil;
-    const int exitStatus = [S7StatusCommand repo:repo calculateStatus:&status];
+    BOOL foundAnyChanges = NO;
+    const int exitStatus = [self printRepoStatus:repo foundAnyChanges:&foundAnyChanges];
+
+    if (NO == foundAnyChanges) {
+        puts("Everything up-to-date");
+    }
+    else {
+        puts("");
+    }
+
+    return exitStatus;
+}
+
+- (int)printRepoStatus:(GitRepository *)repo foundAnyChanges:(BOOL *)foundAnyChanges {
+    NSDictionary<NSString *, NSNumber * /* S7Status */> *subrepoPathToStatus = nil;
+    const int exitStatus = [S7StatusCommand repo:repo calculateStatus:&subrepoPathToStatus];
     if (0 != exitStatus) {
         if (S7ExitCodeSubreposNotInSync == exitStatus) {
             fprintf(stderr,
@@ -78,52 +92,43 @@
         return exitStatus;
     }
 
-    BOOL foundAnyChanges = NO;
+    // don't think that order really matters to end users, but it does matter in tests,
+    // so let output be always sorted in some way
+    NSArray<NSString *> *sortedSubrepoPaths = [subrepoPathToStatus.allKeys sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)];
 
-    NSSet<NSString *> *stagedAddedSubrepos = status[@(S7StatusAdded)];
-    NSSet<NSString *> *stagedDeletedSubrepos = status[@(S7StatusRemoved)];
-    NSSet<NSString *> *stagedUpdatedSubrepos = status[@(S7StatusUpdatedAndRebound)];
+    for (NSString *subrepoPath in sortedSubrepoPaths) {
+        const S7Status status = subrepoPathToStatus[subrepoPath].unsignedIntegerValue;
+        if (0 != (status & (S7StatusAdded | S7StatusRemoved | S7StatusUpdatedAndRebound))) {
+            if (NO == *foundAnyChanges) {
+                *foundAnyChanges = YES;
+                puts("Changes to be committed:");
+            }
 
-    if (stagedAddedSubrepos.count > 0 || stagedDeletedSubrepos.count > 0 || stagedUpdatedSubrepos.count > 0) {
-        foundAnyChanges = YES;
-
-        puts("Changes to be committed:");
-        for (NSString *subrepoPath in stagedAddedSubrepos) {
-            fprintf(stdout, " \033[32madded       %s\033[0m\n", subrepoPath.fileSystemRepresentation);
-        }
-
-        for (NSString *subrepoPath in stagedDeletedSubrepos) {
-            fprintf(stdout, " \033[31mremoved     %s\033[0m\n", subrepoPath.fileSystemRepresentation);
-        }
-
-        for (NSString *subrepoPath in stagedUpdatedSubrepos) {
-            fprintf(stdout, " \033[34mupdated     %s\033[0m\n", subrepoPath.fileSystemRepresentation);
+            if (status & S7StatusAdded) {
+                fprintf(stdout, " \033[32madded       %s\033[0m\n", subrepoPath.fileSystemRepresentation);
+            }
+            else if (status & S7StatusRemoved) {
+                fprintf(stdout, " \033[31mremoved     %s\033[0m\n", subrepoPath.fileSystemRepresentation);
+            }
+            else if (status & S7StatusUpdatedAndRebound) {
+                fprintf(stdout, " \033[34mupdated     %s\033[0m\n", subrepoPath.fileSystemRepresentation);
+            }
         }
     }
 
-    NSSet<NSString *> *notStagedCommittedSubrepos = status[@(S7StatusHasNotReboundCommittedChanges)];
-    NSSet<NSString *> *notStagedUncommittedChangesSubrepos = status[@(S7StatusHasUncommittedChanges)];
-    NSSet<NSString *> *subreposInDetachedHEAD = status[@(S7StatusDetachedHead)];
-
-    S7Config *actualConfig = [[S7Config alloc] initWithContentsOfFile:S7ConfigFileName];
-
     BOOL foundAnyNotReboundChanges = NO;
-    for (S7SubrepoDescription *subrepoDesc in actualConfig.subrepoDescriptions) {
-        NSString *subrepoPath = subrepoDesc.path;
+    for (NSString *subrepoPath in sortedSubrepoPaths) {
+        const S7Status status = subrepoPathToStatus[subrepoPath].unsignedIntegerValue;
 
-        const BOOL hasUncommitedChanges = [notStagedUncommittedChangesSubrepos containsObject:subrepoPath];
-        const BOOL hasCommittedChangesNotReboundInMainRepo = [notStagedCommittedSubrepos containsObject:subrepoPath];
-        const BOOL detachedHEAD = [subreposInDetachedHEAD containsObject:subrepoPath];
-
-        if (NO == hasUncommitedChanges && NO == hasCommittedChangesNotReboundInMainRepo && NO == detachedHEAD) {
+        if (0 == (status & (S7StatusDetachedHead | S7StatusHasUncommittedChanges | S7StatusHasNotReboundCommittedChanges))) {
             continue;
         }
 
         if (NO == foundAnyNotReboundChanges) {
             foundAnyNotReboundChanges = YES;
 
-            if (NO == foundAnyChanges) {
-                foundAnyChanges = YES;
+            if (NO == *foundAnyChanges) {
+                *foundAnyChanges = YES;
             }
             else {
                 puts("");
@@ -132,32 +137,25 @@
             puts("Subrepos not staged for commit:");
         }
 
-        if (detachedHEAD) {
+        if (S7StatusDetachedHead & status) {
             fprintf(stdout, " \033[31;1mdetached HEAD %s\033[0m\n", subrepoPath.fileSystemRepresentation);
         }
-        else if (hasUncommitedChanges && hasCommittedChangesNotReboundInMainRepo) {
+        else if ((S7StatusHasUncommittedChanges & status) && (S7StatusHasNotReboundCommittedChanges & status)) {
             fprintf(stdout, " \033[36mnot rebound commit(s) + uncommitted changes %s\033[0m\n", subrepoPath.fileSystemRepresentation);
         }
-        else if (hasUncommitedChanges) {
+        else if (S7StatusHasUncommittedChanges & status) {
             fprintf(stdout, " \033[35muncommitted changes       %s\033[0m\n", subrepoPath.fileSystemRepresentation);
         }
-        else {
-            NSAssert(hasCommittedChangesNotReboundInMainRepo, @"");
+        else if (S7StatusHasNotReboundCommittedChanges & status) {
             fprintf(stdout, " \033[33;1mnot rebound commit(s)   %s\033[0m\n", subrepoPath.fileSystemRepresentation);
         }
-    }
-
-    if (NO == foundAnyChanges) {
-        puts("Everything up-to-date");
-    }
-    else {
-        puts("");
     }
 
     return S7ExitCodeSuccess;
 }
 
-+ (int)repo:(GitRepository *)repo calculateStatus:(NSDictionary<NSNumber * /* S7Status */, NSSet<NSString *> *> * _Nullable __autoreleasing * _Nonnull)ppStatus {
++ (int)repo:(GitRepository *)repo calculateStatus:(NSDictionary<NSString *, NSNumber * /* S7Status */> * _Nullable __autoreleasing * _Nonnull)ppStatus
+{
     if (NO == [NSFileManager.defaultManager contentsEqualAtPath:S7ConfigFileName andPath:S7ControlFileName]) {
         return S7ExitCodeSubreposNotInSync;
     }
@@ -185,23 +183,11 @@
         return diffExitStatus;
     }
 
-    NSMutableDictionary<NSNumber * /* S7Status */, NSSet<NSString *> *> * status = [NSMutableDictionary new];
+    NSMutableDictionary<NSString *, NSNumber * /* S7Status */> * result = [NSMutableDictionary new];
 
-    if (stagedAddedSubrepos.count > 0) {
-        status[@(S7StatusAdded)] = [NSSet setWithArray:stagedAddedSubrepos.allKeys];
+    for (NSString *subrepoPath in stagedDeletedSubrepos) {
+        result[subrepoPath] = @(S7StatusRemoved);
     }
-
-    if (stagedDeletedSubrepos.count > 0) {
-        status[@(S7StatusRemoved)] = [NSSet setWithArray:stagedDeletedSubrepos.allKeys];
-    }
-
-    if (stagedUpdatedSubrepos.count > 0) {
-        status[@(S7StatusUpdatedAndRebound)] = [NSSet setWithArray:stagedUpdatedSubrepos.allKeys];
-    }
-
-    NSMutableSet<NSString *> *subreposInDetachedHEAD = [NSMutableSet new];
-    NSMutableSet<NSString *> *subreposWithUncommittedChanges = [NSMutableSet new];
-    NSMutableSet<NSString *> *subreposWithCommittedNotReboundChanges = [NSMutableSet new];
 
     for (S7SubrepoDescription *subrepoDesc in actualConfig.subrepoDescriptions) {
         NSString *subrepoPath = subrepoDesc.path;
@@ -210,6 +196,20 @@
         if (nil == subrepoGit) {
             fprintf(stderr, "error: '%s' is not a git repository\n", subrepoPath.fileSystemRepresentation);
             return S7ExitCodeSubrepoIsNotGitRepository;
+        }
+
+        S7Status status = S7StatusUnchanged;
+
+        if (stagedAddedSubrepos[subrepoPath]) {
+            status |= S7StatusAdded;
+        }
+
+        if (stagedDeletedSubrepos[subrepoPath]) {
+            status |= S7StatusRemoved;
+        }
+
+        if (stagedUpdatedSubrepos[subrepoPath]) {
+            status |= S7StatusUpdatedAndRebound;
         }
 
         NSString *currentBranch = nil;
@@ -221,7 +221,7 @@
 
         if (nil == currentBranch) {
             if (isDetachedHEAD) {
-                [subreposInDetachedHEAD addObject:subrepoPath];
+                status |= S7StatusDetachedHead;
             }
             else {
                 fprintf(stderr,
@@ -229,47 +229,34 @@
                         subrepoPath.fileSystemRepresentation);
                 return S7ExitCodeGitOperationFailed;
             }
-            continue;
+        }
+        else {
+            NSString *currentRevision = nil;
+            if (0 != [subrepoGit getCurrentRevision:&currentRevision]) {
+                return S7ExitCodeGitOperationFailed;
+            }
+
+            S7SubrepoDescription *currentSubrepoDesc = [[S7SubrepoDescription alloc]
+                                                        initWithPath:subrepoPath
+                                                        url:subrepoDesc.url
+                                                        revision:currentRevision
+                                                        branch:currentBranch];
+
+            const BOOL hasCommittedChangesNotReboundInMainRepo = (NO == [currentSubrepoDesc isEqual:subrepoDesc]);
+            if (hasCommittedChangesNotReboundInMainRepo) {
+                status |= S7StatusHasNotReboundCommittedChanges;
+            }
+
+            if ([subrepoGit hasUncommitedChanges]) {
+                status |= S7StatusHasUncommittedChanges;
+            }
         }
 
-        NSString *currentRevision = nil;
-        gitExitStatus = [subrepoGit getCurrentRevision:&currentRevision];
-        if (0 != gitExitStatus) {
-            return S7ExitCodeGitOperationFailed;
-        }
-
-        S7SubrepoDescription *currentSubrepoDesc = [[S7SubrepoDescription alloc]
-                                                    initWithPath:subrepoPath
-                                                    url:subrepoDesc.url
-                                                    revision:currentRevision
-                                                    branch:currentBranch];
-
-        const BOOL hasCommittedChangesNotReboundInMainRepo = (NO == [currentSubrepoDesc isEqual:subrepoDesc]);
-        const BOOL hasUncommitedChanges = [subrepoGit hasUncommitedChanges];
-
-        if (hasUncommitedChanges) {
-            [subreposWithUncommittedChanges addObject:subrepoPath];
-        }
-
-        if (hasCommittedChangesNotReboundInMainRepo) {
-            [subreposWithCommittedNotReboundChanges addObject:subrepoPath];
-        }
+        result[subrepoPath] = @(status);
     }
 
-    if (subreposWithUncommittedChanges.count > 0) {
-        status[@(S7StatusHasUncommittedChanges)] = subreposWithUncommittedChanges;
-    }
+    *ppStatus = result;
 
-    if (subreposWithCommittedNotReboundChanges.count > 0) {
-        status[@(S7StatusHasNotReboundCommittedChanges)] = subreposWithCommittedNotReboundChanges;
-    }
-
-    if (subreposInDetachedHEAD.count > 0) {
-        status[@(S7StatusDetachedHead)] = subreposInDetachedHEAD;
-    }
-
-    *ppStatus = status;
-    
     return S7ExitCodeSuccess;
 }
 
