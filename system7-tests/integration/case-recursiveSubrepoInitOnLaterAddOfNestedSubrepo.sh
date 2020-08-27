b0aed59631acfabebe82ef629682cbb9b86b60cf
Pavlo Shkrabliuk <pastey@readdle.com>
2020-08-27 17:05:37 +0300

    remove accidentially committed commented out crap

--- system7-tests/integration/case-recursiveSubrepoInitOnLaterAddOfNestedSubrepo.sh
+++ system7-tests/integration/case-recursiveSubrepoInitOnLaterAddOfNestedSubrepo.sh
@@ -11,33 +11,6 @@ popd
 rm -rf tmp
 
 
-#assert git clone github/rdpdfkit pastey/rdpdfkit
-#
-#cd pastey/rdpdfkit
-#
-#assert s7 init
-#assert git add .
-#assert git commit -m "\"init s7\""
-#
-#assert s7 add --stage Dependencies/FormCalc '"$S7_ROOT/github/FormCalc"'
-#assert git commit -m '"add FormCalc subrepo"'
-#
-#pushd Dependencies/FormCalc > /dev/null
-#  echo AST > Parser.c
-#  git add Parser.c
-#  git commit -m"parser"
-#popd > /dev/null
-#
-#assert s7 rebind --stage
-#
-#assert git commit -m '"up FormCalc"'
-#
-#assert git push --all
-#
-#echo
-#echo
-#echo
-
 cd "$S7_ROOT"
 
 git clone github/rd2 pastey/rd2
