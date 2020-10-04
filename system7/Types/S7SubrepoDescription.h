2dc64f85636b1ef22889505063184d2c299aa5a5
Pavlo Shkrabliuk <pastey@readdle.com>
2020-10-04 20:58:14 +0300

    speed up checkout

--- system7/Types/S7SubrepoDescription.h
+++ system7/Types/S7SubrepoDescription.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface S7SubrepoDescription : NSObject
+@interface S7SubrepoDescription : NSObject<NSCopying>
 
 - (instancetype)init NS_UNAVAILABLE;
 + (instancetype)new NS_UNAVAILABLE;
