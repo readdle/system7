98aa41eda413a96e918387baec84f224af4eccbc
Pavlo Shkrabliuk <pastey@readdle.com>
2023-08-23 22:56:41 +0300

    do not try to remove /usr/local/bin/s7 that we do not install. Cleanup uninstall scripts

--- install.sh
+++ install.sh
@@ -19,11 +19,14 @@ SYSTEM7_DIR="${HOME}/.system7"
 
 function bootstrap() {
     rm -rf "${SYSTEM7_DIR}" > /dev/null
-    rm -f "/usr/local/bin/s7" > /dev/null
+
     rm -f "/usr/local/bin/install-s7.sh" > /dev/null
+    rm -f "/usr/local/bin/uninstall-s7.sh" > /dev/null
     rm -f "/usr/local/bin/update-s7.sh" > /dev/null
+
     rm -f "${HOME}/bin/s7" > /dev/null
     rm -f "${HOME}/bin/install-s7.sh" > /dev/null
+    rm -f "${HOME}/bin/uninstall-s7.sh" > /dev/null
     rm -f "${HOME}/bin/update-s7.sh" > /dev/null
 
 
