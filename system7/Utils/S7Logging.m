a0234afc5b72c8a28b1ea5ec78fbc37c60ff85ce
Roxane M <roxane654@gmail.com>
2025-09-29 18:24:39 +0300

    fixed "format mismatch" error

--- system7/Utils/S7Logging.m
+++ system7/Utils/S7Logging.m
@@ -50,7 +50,7 @@ void logInfo(const char * __restrict format, ...) {
 void logError(const char * __restrict format, ...) {
     va_list va_args;
     va_start(va_args, format);
-    NSString *const nsFormat = [[NSString alloc] initWithCString:format encoding:NSUTF8StringEncoding];
+    NSString *const nsFormat = [NSString stringWithFormat:@"%@", [NSString stringWithUTF8String:format]];
     NSString *const message = [[NSString alloc] initWithFormat:nsFormat arguments:va_args];
     va_end(va_args);
 
